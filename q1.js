49fab7e62dbe2ae364de93acad69523098375bea
akshayhp2298 <akshayhp2298@gmail.com>
2019-12-25 14:26:13 +0530

    imporove in compare two objects

--- q1.js
+++ q1.js
@@ -1,12 +1,17 @@
 function compareObj(obj1,obj2) {
-    let a = JSON.stringify(obj1)
-    let b = JSON.stringify(obj2)
-    return a == b
+    let a = Object.getOwnPropertyNames(obj1)
+    let b = Object.getOwnPropertyNames(obj2)
+    if(a.length!==b.length)return false
+    a.forEach(element=>{
+        if(obj1[element] !==obj2[element])
+        return false
+    })
+    return true
 }
 const x = {
     a:'1',b:2
 }
 const y = {
-    a:'1',b:2
+    b:'1',a:2
 }
 console.log(compareObj(x,y))
